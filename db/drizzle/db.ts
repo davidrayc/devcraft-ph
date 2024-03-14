0bd90e961de78ef76324ae64f506a783f47df022
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-03-14 19:41:47 +0800

    fix: changed db.ts

--- db/drizzle/db.ts
+++ db/drizzle/db.ts
@@ -1,28 +1,13 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
-import { admins } from './schema/admins';
-import { audit_histories } from './schema/audit_histories';
-import { employees } from './schema/employees';
-import { items } from './schema/items';
-import { managers } from './schema/managers';
-import { users } from './schema/users';
+import { Pool } from 'pg';
 
-export const client = new Client({
-  host: process.env.DB_HOST!,
-  port: Number(process.env.DB_PORT!),
-  user: process.env.DB_USERNAME!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
+const pool = new Pool({
+  host: process.env.DATABASE_HOST!,
+  port: Number(process.env.DATABASE_PORT!),
+  user: process.env.DATABASE_USER!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_NAME!,
 });
 
 // { schema } is used for relational queries
-export const db = drizzle(client, {
-  schema: {
-    ...admins,
-    ...audit_histories,
-    ...employees,
-    ...items,
-    ...managers,
-    ...users,
-  },
-});
+export const db = drizzle(pool);

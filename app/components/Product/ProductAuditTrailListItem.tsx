cdec7fb73dcdff7ac2e66a7807e0195d5b6fba64
libmartinito <lib.martinito@pm.me>
2024-03-15 17:32:25 +0800

    style: update line in audit trail to be thinner

--- app/components/Product/ProductAuditTrailListItem.tsx
+++ app/components/Product/ProductAuditTrailListItem.tsx
@@ -13,9 +13,9 @@ const ProductAuditTrailListItem = ({ index, item, totalItemCount }: ProductAudit
   return (
     <li className="flex gap-x-4 items-center h-12">
       <div className="flex flex-col h-full items-center">
-        <div className={`grow ${index === 0 ? 'bg-none' : 'bg-gray-300'} w-1`}></div>
+        <div className={`grow ${index === 0 ? 'bg-none' : 'bg-gray-300'} w-px`}></div>
         <div className="rounded-full bg-gray-300 w-3 h-3 my-1"></div>
-        <div className={`grow ${index === totalItemCount ? 'bg-none' : 'bg-gray-300'} w-1`}></div>
+        <div className={`grow ${index === totalItemCount ? 'bg-none' : 'bg-gray-300'} w-px`}></div>
       </div>
 
       <div>

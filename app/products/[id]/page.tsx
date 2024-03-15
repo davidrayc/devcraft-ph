425f3bc615e2282435a4a92a5ea5d2bb4c0dea77
libmartinito <lib.martinito@pm.me>
2024-03-15 16:42:36 +0800

    refactor: centralize and add structure to dummy data

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,21 +1,42 @@
 import ProductDetails from '../../components/Product/ProductDetails'
 import ProductAuditTrailListItem from '../../components/Product/ProductAuditTrailListItem'
 
-export default function ProductPage({ params }: { params: { id: string } }) {
-  const productAuditTrailListItems = [
-    {
-      id: 0,
-      description: "User A decreased the quantity of product A"
-    },
-    {
-      id: 1,
-      description: "User B decreased the quantity of product A"
-    },
-    {
-      id: 2,
-      description: "User C decreased the quantity of product A"
-    },
-  ]
+type AuditTrailListItem = {
+  id: number;
+  description: string;
+}
+
+type Product = {
+  id: number;
+  name: string;
+  itemCode: string;
+  quantity: number;
+  auditTrailListItems: AuditTrailListItem[];
+}
+
+export default function ProductPage() {
+  const dummyProduct: Product = {
+    id: 0,
+    name: "Product Name",
+    itemCode: "_item_code",
+    quantity: 0,
+    auditTrailListItems: [
+      {
+        id: 0,
+        description: "User A decreased the quantity of product A"
+      },
+      {
+        id: 1,
+        description: "User B decreased the quantity of product A"
+      },
+      {
+        id: 2,
+        description: "User C decreased the quantity of product A"
+      },
+    ]
+  }
+
+  const { name, itemCode, quantity, auditTrailListItems } = dummyProduct
 
   return (
     <div>
@@ -24,16 +45,15 @@ export default function ProductPage({ params }: { params: { id: string } }) {
           <div className="rounded-xl border border-gray-300 w-80 h-80"></div>
 
           <div>
-            <div className="text-6xl font-bold">Product Name</div>
-
-            <ProductDetails itemCode="_item_code" quantity={0} />
+            <div className="text-6xl font-bold">{name}</div>
+            <ProductDetails itemCode={itemCode} quantity={quantity} />
           </div>
         </div>
 
         <div className="rounded-xl border border-gray-300 text-xl max-w-4xl w-full mt-12 py-12 px-20">
           <ul className="flex flex-col w-full">
-            {productAuditTrailListItems.map((item, index) => (
-              <ProductAuditTrailListItem key={item.id} index={index} item={item} totalItemCount={productAuditTrailListItems.length - 1} />
+            {auditTrailListItems.map((item, index) => (
+              <ProductAuditTrailListItem key={item.id} index={index} item={item} totalItemCount={auditTrailListItems.length - 1} />
             ))}
           </ul>
         </div>

17054264ed51a1c2e6875fa7b9edce07c8b1325f
dove <codegeekdove@gmail.com>
2024-03-16 18:33:00 +0800

    fix: modal behavior

--- app/components/CreateUserForm/index.tsx
+++ app/components/CreateUserForm/index.tsx
@@ -17,9 +17,9 @@ export default function CreateUserForm() {
               <div className="mt-2">
                 <input
                   type="text"
-                  name="email"
                   id="email"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  // {...register('email', { required: 'Please add an email' })}
                 />
               </div>
             </div>
@@ -33,9 +33,11 @@ export default function CreateUserForm() {
               <div className="mt-2">
                 <input
                   type="text"
-                  name="password"
                   id="password"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  // {...register('password', {
+                  //   required: 'Password is required',
+                  // })}
                 />
               </div>
             </div>
--- app/components/Modal/index.tsx
+++ app/components/Modal/index.tsx
@@ -8,15 +8,18 @@ export default function Modal() {
   const modal = searchParams.get('createuser');
   const pathname = usePathname();
 
-  function handleClick() {
-    router.push(pathname);
+  function handleClick(e: React.MouseEvent<HTMLDivElement>) {
+    const target = e.target as HTMLElement;
+    if (target.classList.contains('overlay')) {
+      router.push(pathname);
+    }
   }
 
   return (
     <>
       {modal && (
         <div
-          className="relative z-10"
+          className="z-9  relative"
           aria-labelledby="modal-title"
           role="dialog"
           aria-modal="true"
@@ -25,7 +28,7 @@ export default function Modal() {
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
           <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full cursor-default items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="overlay flex min-h-full cursor-default items-end justify-center p-4 text-center sm:items-center sm:p-0">
               <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <CreateUserForm />

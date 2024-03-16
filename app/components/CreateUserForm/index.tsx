d0d264dee84c5788e14e8a09c0e5a57799e76aed
dove <codegeekdove@gmail.com>
2024-03-16 17:52:28 +0800

    feate: add form to the modal

---
+++ app/components/CreateUserForm/index.tsx
@@ -0,0 +1,55 @@
+export default function CreateUserForm() {
+  return (
+    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+      <h3 className="text-base font-semibold leading-6 text-gray-900">
+        Create user
+      </h3>
+      <div>
+        <form>
+          <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-6">
+            <div className="col-span-full">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="col-span-full">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="password"
+                  id="password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="col-span-full bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse">
+              <button
+                type="submit"
+                className="rounded-full bg-blue-500 px-4 py-2 font-bold capitalize text-white hover:bg-blue-700"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- app/components/Modal/index.tsx
+++ app/components/Modal/index.tsx
@@ -1,44 +1,39 @@
 'use client';
-import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import CreateUserForm from '../CreateUserForm';
 
-export default function Modal({ children }: React.ReactNode) {
+export default function Modal() {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const modal = searchParams.get('createuser');
   const pathname = usePathname();
 
+  function handleClick() {
+    router.push(pathname);
+  }
+
   return (
     <>
       {modal && (
-        <Link
+        <div
           className="relative z-10"
           aria-labelledby="modal-title"
           role="dialog"
           aria-modal="true"
-          href={pathname}
+          onClick={handleClick}
         >
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
           <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="flex min-h-full cursor-default items-end justify-center p-4 text-center sm:items-center sm:p-0">
               <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <h3
-                        className="text-base font-semibold leading-6 text-gray-900"
-                        id="modal-title"
-                      >
-                        Create user
-                      </h3>
-                      <div className="mt-2">{children}</div>
-                    </div>
-                  </div>
+                  <CreateUserForm />
                 </div>
               </div>
             </div>
           </div>
-        </Link>
+        </div>
       )}
     </>
   );

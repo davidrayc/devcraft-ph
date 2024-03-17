ef9728f1f8968b1c7ddd53a53a43297d13083ef5
Paolo Venegas <paolo.venegas@reinteractive.com>
2024-03-17 16:38:03 +0800

    feat: add seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO items(code, name, date, description, image_path) VALUES('WX4D2', 'Lava lamp', '2024-03-17', 'Retro lighting acccessory', 'path/to/lava-lamp.jpg');
+
+INSERT INTO users(employee_id, first_name, last_name, email, password) VALUES('GX0001', 'Bob', 'Builder', 'bob@builders.ph', '***');
+
+INSERT INTO audit_histories(item_id, user_id) VALUES(1,1);

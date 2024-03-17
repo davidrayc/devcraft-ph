0d72c150f0da468a1fc529e87ea3af2965ceff67
Paolo Venegas <paolo.venegas@reinteractive.com>
2024-03-17 16:40:39 +0800

    feat: start items API endpoint

---
+++ app/api/v1/items/[id]/route.ts
@@ -0,0 +1,48 @@
+import { db } from '@/db/drizzle/db';
+import { items } from '@/db/drizzle/schema/items';
+import { eq } from 'drizzle-orm';
+
+import { notFound } from "next/navigation";
+
+export const dynamic = 'force-dynamic';
+
+export async function GET(request: Request, { params }: { params: { id: number } }) {
+  const id = parseInt(params.id);
+
+  // TODO: consider moving queries into a library
+
+  // Query using select
+  // const result = await db.select().from(items).where(eq(items.id, id));
+
+  // Query using findFirst
+  const result = await db.query.items.findFirst({
+    where: (items, { eq }) => eq(items.id, id),
+    with: {
+      // Include relation
+      // with: {
+      //   auditHistories: true,
+      // }
+
+      // Include nested relations
+      auditHistories: {
+        with: {
+          // Select all
+          // user: true,
+
+          // Exclude column
+          user: {
+            columns: {
+              password: false,
+            }
+          },
+        },
+      },
+    },
+  });
+
+  if (result)
+    return Response.json(result);
+  end
+
+  return new Response(null, { status: 404 })
+}

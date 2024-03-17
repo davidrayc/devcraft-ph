197556856e66edaa391b80fc8b7422d70ea092d2
auroradream04 <alvs131313@gmail.com>
2024-03-18 01:36:47 +0800

    fix: reverted code's type back to varchar

--- db/drizzle/schema/item.ts
+++ db/drizzle/schema/item.ts
@@ -1,12 +1,12 @@
 import { relations } from 'drizzle-orm';
-import { date, integer, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { date, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 import { audit_history } from './audit_history';
 
 export const item = pgTable('item', {
   id: serial('id').primaryKey().notNull(),
   item_code: varchar('code', { length: 10 }).notNull(),
   item_name: varchar('name', { length: 256 }).notNull(),
-  quantity: integer('quantity').notNull(),
+  quantity: varchar('quantity', { length: 10 }).notNull(),
   ageing: date('date'),
   description: text('description'),
   img_path: varchar('image_path', { length: 256 }),

6e964b2707ab43c0ff602450ee53c86f1dc4c449
auroradream04 <alvs131313@gmail.com>
2024-03-19 23:40:18 +0800

    fix: change quantity's type to integer and description to varchar

--- db/drizzle/schema/item.ts
+++ db/drizzle/schema/item.ts
@@ -1,14 +1,14 @@
 import { relations } from 'drizzle-orm';
-import { date, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { date, integer, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 import { audit_history } from './audit_history';
 
 export const item = pgTable('item', {
   id: serial('id').primaryKey().notNull(),
   code: varchar('code', { length: 10 }).notNull(),
   name: varchar('name', { length: 256 }).notNull(),
-  quantity: varchar('quantity', { length: 10 }).notNull(),
+  quantity: integer('quantity').notNull(),
   ageing: date('date'),
-  description: text('description'),
+  description: varchar('description', { length: 256 }),
   img_path: varchar('image_path', { length: 256 }),
 });
 

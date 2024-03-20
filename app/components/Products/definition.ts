50b2b0f5d7f2e83f5951bf2af86313ae407f3cb6
auroradream04 <alvs131313@gmail.com>
2024-03-20 23:32:15 +0800

    chore: add definition for GetComponentProps and PostComponentProps

--- app/components/Products/definition.ts
+++ app/components/Products/definition.ts
@@ -3,3 +3,18 @@ export type Product = {
     code: string,
     quantity: number
 }
+
+export type GetComponentProps = {
+    setRealItemCode: (code: string) => void;
+    isGetValid: boolean;
+    handleGet: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    handleGetAll: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export type PostComponentProps = {
+    setProductName: (name: string) => void;
+    setItemCode: (code: string) => void;
+    setQuantity: (quantity: number) => void;
+    handlePost: (e: React.FormEvent<HTMLFormElement>) => void;
+    isPostValid: boolean;
+}

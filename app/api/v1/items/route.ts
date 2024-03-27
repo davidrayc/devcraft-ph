2fc381fe949da6727e7c879e9e0ff664d03151c4
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-03-27 20:50:50 +0800

    feat: insert item to db
    - date-fns package
    - itemProps type definitions

--- app/api/v1/items/route.ts
+++ app/api/v1/items/route.ts
@@ -1,4 +1,8 @@
 import { db } from '@/db/drizzle/db';
+import { item } from '@/db/drizzle/schema/item';
+import { format } from 'date-fns';
+import { ItemProps } from './definitions';
+
 export const dynamic = 'force-dynamic';
 
 //FETCH ALL ITEMS FROM DB
@@ -12,6 +16,33 @@ export async function GET(request: Request) {
     limit: pageLimit,
     offset: (page - 1) * pageLimit,
   });
+
+  if (result) return Response.json(result);
+  return new Response(null, { status: 404 });
+}
+
+//INSERT ITEM TO DB
+export async function POST(request: Request) {
+  const requestItem: ItemProps = await request.json();
+
+  const insertItem = async (itemDetails: ItemProps) => {
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, 'yyyy-MM-dd');
+
+    return db
+      .insert(item)
+      .values({
+        name: itemDetails.name,
+        code: itemDetails.code,
+        quantity: itemDetails.quantity,
+        ageing: formattedDate,
+        description: itemDetails.description,
+        img_path: itemDetails.img_path,
+      })
+      .returning();
+  };
+
+  const result = await insertItem(requestItem);
 
   if (result) return Response.json(result);
   return new Response(null, { status: 404 });

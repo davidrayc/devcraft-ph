0b4a573dc9c523712c23074cd715f61e4d8343a5
CedricAngeloSina-on <cao.sinaon@gmail.com>
2024-03-27 20:51:51 +0800

    feat: update item by id

--- app/api/v1/items/[id]/route.ts
+++ app/api/v1/items/[id]/route.ts
@@ -1,4 +1,9 @@
 import { db } from '@/db/drizzle/db';
+import { item } from '@/db/drizzle/schema/item';
+import { format } from 'date-fns';
+import { eq } from 'drizzle-orm';
+import { ItemProps } from '../definitions';
+
 export const dynamic = 'force-dynamic';
 
 //FETCH ITEM FROM DB BY ID
@@ -19,6 +24,38 @@ export async function GET(
       },
     },
   });
+
+  if (result) return Response.json(result);
+  return new Response(null, { status: 404 });
+}
+
+//UPDATE ITEM FROM DB BY ID
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: number } },
+) {
+  const id = params.id;
+  const requestItem: ItemProps = await request.json();
+
+  const updateItem = async (itemDetails: ItemProps) => {
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, 'yyyy-MM-dd');
+
+    return db
+      .update(item)
+      .set({
+        name: itemDetails.name,
+        code: itemDetails.code,
+        quantity: itemDetails.quantity,
+        ageing: formattedDate,
+        description: itemDetails.description,
+        img_path: itemDetails.img_path,
+      })
+      .where(eq(item.id, id))
+      .returning();
+  };
+
+  const result = await updateItem(requestItem);
 
   if (result) return Response.json(result);
   return new Response(null, { status: 404 });
